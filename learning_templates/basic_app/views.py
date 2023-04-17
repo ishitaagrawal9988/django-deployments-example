3afe51e575bfa768913eb3a832c522ec5de35321
ishitaagrawal9988 <ishitaagrawal9988@gmail.com>
2023-04-17 18:14:40 +0530

    first commit

---
+++ learning_templates/basic_app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    contex_dict={'text':'hello world','number':100}
+    return render(request,'basic_app/index.html',contex_dict)
+
+def basic(request):
+    return render(request,'basic_app/basic.html')
+
+def other(request):
+    return render(request,'basic_app/other.html')
+
+def relative_url_template(request):
+    return render(request,'basic_app/relative_url_template.html')
